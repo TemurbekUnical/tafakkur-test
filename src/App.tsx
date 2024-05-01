a344827c461db03a5de1566b0eb32b48d3fffb23
Temurbek <temurbekunical@gmail.com>
2024-05-01 22:27:46 +0500

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import AppLayout from "./layouts/AppLayout";
 import Product from "./pages/product/page";
-import Category from "./pages/category/page";
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to={"/products"} />} />
         <Route path="/products" element={<Product />} />
-        <Route path="/category" element={<Category />} />
       </Routes>
     </AppLayout>
   );
