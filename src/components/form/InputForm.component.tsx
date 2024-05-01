290673f8e7597b61ece338786bb63aa83360d55a
Temurbek <temurbekunical@gmail.com>
2024-05-01 22:38:13 +0500

    ok

--- src/components/form/InputForm.component.tsx
+++ src/components/form/InputForm.component.tsx
@@ -24,7 +24,7 @@ function InputForm<FormNames extends Record<string, any>>({
   label = "",
   disabled = false,
   placeholder = "Введите информацию",
-  className = "",
+  className = " ",
   control,
   name,
   required,
